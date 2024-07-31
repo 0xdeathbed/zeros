70ad095e8df7a6db8502f619e28f58a3e7ce757c
Paras <161898004+paradoxedmind@users.noreply.github.com>
2024-07-31 15:04:20 +0530

    01-minimal-kernel

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # zeros
+Operating System or Kernel in Rust
+
+
+## 1. Minimal Kernel
+
+Creating a minimal rust binary which does can be run without underlaying operating system, Also known as "freestanding" or "bare-metal" executable. Meaning losing ability to use `Rust Standard Library` but still able to use most of rust features eg., `iterators`,`closures`,`pattern matching`,`Enums (including Option and Result)`,`String formating`,`ownership system`,etc.
+
+Our kernel is should run on bare-metal `x86-64` system. So we need to compile to a suitable target triple. And target should not have any underlying operating system. We can get this suitable triple `x86-64-unknown-none` by using rustup.
+
+```bash
+cargo build --target x86_64-unknown-none # --relase
+  
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std] // Disable std linking
+#![no_main] // To disable normal entry point i.e `main`
+
+use core::panic::PanicInfo;
+
+#[no_mangle] // Disable name mangling to ensure `_start` remains function name
+             // `_start` used because it's default entry point name for most systems
+pub extern "C" fn _start() -> ! {
+    // As this function is invoked by os or bootloader it should invoke exit syscall or shutdown machine instead of returning so marked as diverging by never type
+
+    loop {}
+}
+
+#[panic_handler] // Defining function to invoke on panic which std automatically provides
+fn panic(_info: &PanicInfo) -> ! {
+    // As panic handler function should never return it is marked as diverging function by
+    // returning never type `!`
+    loop {}
 }
